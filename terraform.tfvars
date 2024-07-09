8b5d9fd510dd294c72a480429c613a371d5d597f
rahib98 <rayhanshaikh2310@gmail.com>
2024-07-09 07:01:51 -0400

    Save current changes

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+# terraform.tfvars
+client_secret = "qX_8Q~wfk4lfXDTfvFSiX.Lw7N8YVj1yBxNiFcsF"
---
+++ variables.tf
@@ -0,0 +1,4 @@
+# variables.tf
+variable "client_secret" {
+  description = "Azure Active Directory Application Client Secret"
+}
